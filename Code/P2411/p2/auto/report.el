97c963f6e488059675dbbfcd9508f22a9818fade
Duncan Wilkie <antigravityd@gmail.com>
2021-09-14 14:58:16 -0500

    Syncing garbage

--- Code/P2411/p2/auto/report.el
+++ Code/P2411/p2/auto/report.el
@@ -1,9 +1,33 @@
 (TeX-add-style-hook
  "report"
  (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
    (TeX-run-style-hooks
     "latex2e"
     "article"
-    "art10"))
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:orgf4520d7"
+    "sec:org8e56bb6"
+    "sec:org517aaca"
+    "sec:org2b59d8a"
+    "sec:orgdb0e617"
+    "sec:orgf7f6b3d"))
  :latex)
 

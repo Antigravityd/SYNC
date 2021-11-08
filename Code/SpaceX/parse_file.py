c70bce18df1ba95e76bb87ca0b5386e0414f8a58
Duncan Wilkie <antigravityd@gmail.com>
2021-11-08 14:22:56 -0600

    random

---
+++ Code/SpaceX/parse_file.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+# for converting hexadecimal to binary
+import binascii
+
+from structures import *
+from pixel_decoder import *
+
+def bytesToInt16(byte1, byte2):
+
+    return byte1<<8 | byte2
+
+def parseFile(infile):
+
+    data_out = []
+
+    for line in infile:
+
+        # cut the endl character
+        hex_data = line[:-1]
+
+        # convert hexadecimal to binary
+        try:
+            data = binascii.unhexlify(hex_data)
+        except:
+            print("Unhexification failed on data: {}".format(hex_data))
+            continue
+
+        # 20 = ID specific to the FrameDataMsg()
+        if data[0] == 20:
+
+            frame_data = FrameDataMsg()
+
+            # extract the bytes
+            # the following ones are not encoded and can be just coppied
+            frame_data.frame_id         = bytesToInt16(data[2], data[1])
+            frame_data.packet_id        = bytesToInt16(data[4], data[3])
+            frame_data.mode             = data[5]
+            frame_data.n_pixels         = data[6]
+            frame_data.checksum_matched = data[7]
+
+            # the following pixel data are encoded and need to be
+            # deserialized and derandomized
+            # for each pixel in the packet
+            for i in range(0, frame_data.n_pixels):
+
+                # list of 6 bytes
+                pixel_data = []
+
+                # for each one of the 6 bytes encoding the pixel data
+                for j in range(0, 6):
+
+                    pixel_data.append(data[8 + i*6 + j])
+
+                # deserialize and derandomize the pixel data
+                # pixel_data should be one of 3 classes
+                # * PixelDataToAToT()
+                # * PixelDataToA()
+                # * PixelDataMpxiToT()
+                pixel_data = convert_packet(pixel_data, 4, frame_data.mode)
+
+                if pixel_data:
+                    frame_data.pixel_data.append(pixel_data)
+
+            data_out.append(frame_data)
+
+    return data_out
---
+++ Code/SpaceX/pixel_decoder.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+from tpx3_luts import *
+from structures import *
+
+def convert_packet(data, colShiftNum, tpx_mode):
+
+    colshifttbl = LUT_COLSHIFTS[colShiftNum]
+
+    address     = (data[0] & 0x0F) << 12 | (data[1] << 4) | ((data[2] >> 4) & 0x0F)
+    value3      = ((data[2] & 0x0F) << 10) | (data[3] << 2) | ((data[4] >> 6) & 0x03)
+    value2      = ((data[4] & 0x3F) << 4) | ((data[5] >> 4) & 0x0F)
+    value1      = (data[5] & 0x0F)
+    eoc         = (address >> 9) & 0x7F
+    sp          = (address >> 3) & 0x3F
+    pix         = address & 0x07
+    x           = eoc * 2 + (pix // 4)
+    y           = (sp * 4 + (pix % 4))
+    idx         = y * 256 + x
+
+    if tpx_mode == LLCP_TPX3_PXL_MODE_TOA_TOT:
+
+        ftoa = (value1 + colshifttbl[x])
+        tot = LUT_TOT[value2] if value2 >= 1 and value2 < MAX_LUT_TOT else WRONG_LUT_TOT
+        toa = LUT_TOA[value3] if value3 >= 1 and value3 < MAX_LUT_TOA else WRONG_LUT_TOA
+
+        data           = PixelDataToAToT()
+        data.x         = x
+        data.y         = y
+        data.toa       = toa
+        data.tot       = tot
+        data.ftoa      = ftoa
+
+
+    elif tpx_mode == LLCP_TPX3_PXL_MODE_TOA:
+
+        ftoa = (value1 + colshifttbl[x])
+        toa = LUT_TOA[value3] if value3 >= 0 and value3 < MAX_LUT_TOA else WRONG_LUT_TOA
+
+        data           = PixelDataToA()
+        data.x         = x
+        data.y         = y
+        data.toa       = toa
+        data.ftoa      = ftoa
+
+    elif tpx_mode == LLCP_TPX3_PXL_MODE_MPX_ITOT:
+
+        mpx = LUT_EVENT[value2] if value2 >= 1 and value2 < MAX_LUT_EVENT else WRONG_LUT_EVENT
+        itot = LUT_ITOT[value3] if value3 >= 1 and value3 < MAX_LUT_ITOT else WRONG_LUT_ITOT
+
+        data           = PixelDataMpxiToT()
+        data.x         = x
+        data.y         = y
+        data.itot      = itot
+        data.mpx       = mpx
+
+    else:
+        print("Error: wrong Timepix3 mode specified, need {0, 1, 2}")
+        return False
+
+    return data

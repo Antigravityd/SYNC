fb6bdd19c9376e8493589dd221bee4f68f012455
Duncan Wilkie <antigravityd@gmail.com>
2022-04-07 09:51:39 -0500

    midterm checkpoint for proof

--- status.sh
+++ status.sh
@@ -17,28 +17,28 @@ while true; do
 	ticker=${ticker:2}
     fi
 
+    cpu_util=($(mpstat -P ALL 1 1 | awk '/Average:/ && $2 ~ /[0-9]/ {print $3}'))
+    # now_time=$(date +%s)
+    # for i in {0..3}
+    # do
+    # 	ln=$((i+2))
+    # 	usage=$(sed "${ln}q;d" /proc/stat)
+    # 	IFS=" " read -ra usage_arr <<< "$usage"
 
-    now_time=$(date +%s)
-    for i in {0..3}
-    do
-	ln=$((i+2))
-	usage=$(sed "${i+2}q;d" /proc/stat)
-	IFS=" " read -ra usage_arr <<< "$usage"
+    # 	now_use=$((usage_arr[1] + usage_arr[2] + usage_arr[3] + usage_arr[6] + usage_arr[7] + usage_arr[8]))
+    # 	now_idle=$((usage_arr[4] + usage_arr[5]))
 
-	now_use=$((usage_arr[1] + usage_arr[2] + usage_arr[3] + usage_arr[6] + usage_arr[7] + usage_arr[8]))
-	now_idle=$((usage_arr[4] + usage_arr[5]))
+    # 	delta_time=$((now_time-last_time))
+    # 	delta_use=$((now_use-last_use[i]))
+    # 	delta_idle=$((now_idle-last_idle[i]))
+    # 	delta_total=$((delta_use+delta_idle))
 
-	delta_time=$((now_time-last_time))
-	delta_use=$((now_use-last_use[i]))
-	delta_idle=$((now_idle-last_idle[i]))
-	delta_total=$((delta_use+delta_idle))
+    # 	last_use[i]=${now_use}
+    # 	last_idle[i]=${now_idle}
 
-	last_use[i]=${now_use}
-	last_idle[i]=${now_idle}
-
-	cpu_util[i]=$(bc -l <<< "100*${delta_use}/${delta_total}/${delta_time}" | cut -c -4)
-    done
-    last_time=$(date +%s)
+    # 	cpu_util[i]=$(bc -l <<< "100*${delta_use}/${delta_total}/${delta_time}" | cut -c -4)
+    # done
+    # last_time=$(date +%s)
     mfree=$(free -m | sed '2q;d' | awk '{print $3}')
     total=$(free -m | sed '2q;d' | awk '{print $2}')
     total=$(bc -l <<< "${total}/1000" | cut -c -4)
@@ -52,7 +52,7 @@ while true; do
 
     dfree=$(btrfs filesystem usage /  2> /dev/null | sed '3q;d' | awk '{print $3}')
 
-    tx=$(ifconfig eno1 | sed '7q;d' | awk '{print $2}' | sed 's/bytes://')
+    tx=$(ifconfig eno1 | sed '7q;d' | awk '{print $4}' | sed 's/bytes://')
     tx_delta=$(($tx-$last_tx))
     last_tx=$tx
     if [ $tx_delta -lt 1000 ]; then
@@ -88,5 +88,4 @@ while true; do
     xsetroot -name "  <span foreground=\"#114477\"></span> ( ${cpu_util[0]}%  ${cpu_util[1]}%  ${cpu_util[2]}%  ${cpu_util[3]}% ) | <span foreground=\"#114477\"></span> ${mfree} | <span foreground=\"#114477\"></span> ${dfree}/1.80TiB | ${tx_delta}<span foreground=\"#114477\"></span> ${rx_delta}<span foreground=\"#114477\"></span>   ${datetime} ; ${weather} ;${ticker} "
 
     ((loop++))
-    sleep 1
 done

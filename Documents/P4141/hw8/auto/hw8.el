b036935dbb9f20a970999bce18ce42df0f677349
Duncan Wilkie <antigravityd@gmail.com>
2022-04-08 10:11:06 -0500

    Quantum HW half done

---
+++ Documents/P4141/hw8/auto/hw8.el
@@ -0,0 +1,16 @@
+(TeX-add-style-hook
+ "hw8"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "letterpaper")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "geometry"
+    "amsmath"
+    "amssymb"
+    "siunitx"
+    "graphicx"))
+ :latex)
+

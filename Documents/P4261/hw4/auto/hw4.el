0db2edce1f06453ad5d3772893daddc26da0783b
Duncan Wilkie <antigravityd@gmail.com>
2022-04-29 09:57:13 -0500

    Syncing a bunch

---
+++ Documents/P4261/hw4/auto/hw4.el
@@ -0,0 +1,18 @@
+(TeX-add-style-hook
+ "hw4"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "letterpaper")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "geometry"
+    "siunitx"
+    "amsmath"
+    "amssymb"
+    "graphicx")
+   (LaTeX-add-environments
+    '("gnuplot" LaTeX-env-args ["argument"] 0)))
+ :latex)
+

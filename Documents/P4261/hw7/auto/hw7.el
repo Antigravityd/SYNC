e981a6dc67b2c91dd47838ac6e4409fa4eeb0f0d
Duncan Wilkie <antigravityd@gmail.com>
2022-05-02 11:56:50 -0500

    lots of solid state

---
+++ Documents/P4261/hw7/auto/hw7.el
@@ -0,0 +1,15 @@
+(TeX-add-style-hook
+ "hw7"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "letterpaper")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "geometry"
+    "amsmath"
+    "siunitx"
+    "graphicx"))
+ :latex)
+

76610bd1e0a62cec499c2550f234fbfb7e3b78c1
Duncan Wilkie <antigravityd@gmail.com>
2022-09-06 08:08:51 -0500

    7210 HW 2

--- Documents/M7210/hw2/auto/hw2.el
+++ Documents/M7210/hw2/auto/hw2.el
@@ -1,14 +1,22 @@
 (TeX-add-style-hook
  "hw2"
  (lambda ()
+   (setq TeX-command-extra-options
+         "-shell-escape")
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("geometry" "letterpaper")))
+   (add-to-list 'LaTeX-verbatim-environments-local "minted")
    (TeX-run-style-hooks
     "latex2e"
     "article"
     "art10"
     "geometry"
     "amsmath"
-    "siunitx"))
+    "amssymb"
+    "amsthm"
+    "minted"
+    "tgpagella")
+   (LaTeX-add-amsthm-newtheorems
+    "prop"))
  :latex)
 

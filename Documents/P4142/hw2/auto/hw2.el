611e37f705024db0d33efe837fe37d52780c32a6
Duncan Wilkie <antigravityd@gmail.com>
2022-09-13 03:34:50 -0500

    7210 HW 3

---
+++ Documents/P4142/hw2/auto/hw2.el
@@ -0,0 +1,19 @@
+(TeX-add-style-hook
+ "hw2"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "letterpaper")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "geometry"
+    "tgpagella"
+    "amsmath"
+    "amssymb"
+    "amsthm"
+    "siunitx")
+   (LaTeX-add-environments
+    "prob"))
+ :latex)
+

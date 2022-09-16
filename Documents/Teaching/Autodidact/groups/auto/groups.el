775b593718b4fbb47f539da6e540dd4b48aa8c33
Duncan Wilkie <antigravityd@gmail.com>
2022-09-16 09:38:31 -0500

    Bruh 2

---
+++ Documents/Teaching/Autodidact/groups/auto/groups.el
@@ -0,0 +1,15 @@
+(TeX-add-style-hook
+ "groups"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "letterpaper")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "geometry"
+    "amsmath"
+    "amssymb"
+    "amsthm"))
+ :latex)
+

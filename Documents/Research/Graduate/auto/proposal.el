290f2c6a1ae2d047f79ff1e0ecda8a69ee456f2e
Duncan Wilkie <antigravityd@gmail.com>
2022-11-01 07:11:19 -0500

    Algebruh

--- Documents/Research/Graduate/auto/proposal.el
+++ Documents/Research/Graduate/auto/proposal.el
@@ -1,8 +1,23 @@
 (TeX-add-style-hook
  "proposal"
  (lambda ()
-   (LaTeX-add-bibitems
-    "dummies"
-    "risk"))
- '(or :bibtex :latex))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "letterpaper") ("fontsize" "fontsize=12pt")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "profiling/hist"
+    "article"
+    "art10"
+    "geometry"
+    "tgpagella"
+    "amsmath"
+    "siunitx"
+    "fontsize"
+    "tikz"
+    "gnuplot-lua-tikz"
+    "wrapfig")
+   (LaTeX-add-labels
+    "sec:project-narrative"
+    "fig:mcruntime"))
+ :latex)
 

b2d3f336a6d51b6573bb5cd96fcdafe4a0d4dadd
Duncan Wilkie <antigravityd@gmail.com>
2022-11-08 07:28:50 -0600

    Algebruh plus extra

---
+++ Documents/Research/Graduate/auto/cv.el
@@ -0,0 +1,28 @@
+(TeX-add-style-hook
+ "cv"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("classicthesis" "nochapters") ("currvita" "LabelsAligned")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "scrartcl"
+    "scrartcl10"
+    "tgpagella"
+    "classicthesis"
+    "currvita"
+    "hyperref")
+   (TeX-add-symbols
+    '("Description" 1)
+    '("NewEntry" 3)
+    '("MarginText" 1))
+   (LaTeX-add-lengths
+    "datebox"))
+ :latex)
+
---
+++ Documents/Research/Math/based/auto/based.el
@@ -0,0 +1,23 @@
+(TeX-add-style-hook
+ "based"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "letterpaper")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "geometry"
+    "tgpagella"
+    "amsmath"
+    "amssymb"
+    "amsthm"
+    "tikz"
+    "minted"
+    "physics"
+    "siunitx"
+    "tikz-cd")
+   (LaTeX-add-environments
+    "thm"))
+ :latex)
+

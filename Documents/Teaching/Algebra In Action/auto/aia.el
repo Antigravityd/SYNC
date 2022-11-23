6e0adfe004f78036c5890eb928f0556971718363
Duncan Wilkie <antigravityd@gmail.com>
2022-11-23 02:21:11 -0600

    Started a textbook

---
+++ Documents/Teaching/Algebra In Action/auto/aia.el
@@ -0,0 +1,22 @@
+(TeX-add-style-hook
+ "aia"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "letterpaper")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "book"
+    "bk10"
+    "geometry"
+    "tgpagella"
+    "amsmath"
+    "amssymb"
+    "amsthm"
+    "tikz"
+    "minted"
+    "physics"
+    "siunitx")
+   (LaTeX-add-environments
+    "plm"))
+ :latex)
+

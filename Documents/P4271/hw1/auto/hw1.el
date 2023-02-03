db0b70489d08fd895702f0502166de6b67610782
Duncan <antigravityd@gmail.com>
2023-02-03 05:24:23 -0600

    Physics 1 redux

---
+++ Documents/P4271/hw1/auto/hw1.el
@@ -0,0 +1,24 @@
+(TeX-add-style-hook
+ "hw1"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "letterpaper")))
+   (add-to-list 'LaTeX-verbatim-environments-local "minted")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "geometry"
+    "tgpagella"
+    "amsmath"
+    "amssymb"
+    "amsthm"
+    "tikz"
+    "minted"
+    "physics"
+    "siunitx"
+    "mhchem")
+   (LaTeX-add-amsthm-newtheorems
+    "plm"))
+ :latex)
+

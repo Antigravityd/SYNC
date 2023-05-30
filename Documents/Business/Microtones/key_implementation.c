5836aa4da42c0a4789644a350d9529ee9409ef61
Duncan <antigravityd@gmail.com>
2023-05-30 18:13:33 -0500

    A couple Roam documents and a kicad alteration

---
+++ Documents/Business/Microtones/key_implementation.c
@@ -0,0 +1,14 @@
+// Current MCU choice: AT90CAN64.
+
+uint16_t key_id = 0;
+uint16_t count = 0;
+
+int main() {
+  // initialize SPI peripheral
+  // register an initial interrupt handler. This should:
+  //     1. interpret the first 16 (?) bits over the bus as an ID assignment
+  //     2.
+
+
+
+}
